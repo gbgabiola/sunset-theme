9abbd07b221a1294c6c30db71b0ca94b9e0c1452
Genesis Gabiola <gbgabiola@gmail.com>
2021-05-15 02:35:35 +0800

    Add admin subpages

--- inc/function-admin.php
+++ inc/function-admin.php
@@ -7,10 +7,22 @@
 	==========================================
 */
 function sunset_add_admin_page() {
-  add_menu_page('Sunset Theme Options', 'Sunset', 'manage_options', 'gbgabiola-sunset', 'sunset_theme_create_page', get_template_directory_uri() . '/img/sunset-icon.png', 110);
+  // Generate Sunset Admin Page
+  add_menu_page('Sunset Theme Options', 'Sunset', 'manage_options', 'gbgabiola_sunset', 'sunset_theme_create_page', get_template_directory_uri() . '/img/sunset-icon.png', 110);
+
+  // Generate Sunset Admin Sub Pages
+  add_submenu_page('gbgabiola_sunset', 'Sunset Theme Options', 'General', 'manage_options', 'gbgabiola_sunset', 'sunset_theme_create_page');
+
+  add_submenu_page('gbgabiola_sunset', 'Sunset CSS Options', 'Custom CSS', 'manage_options', 'gbgabiola_sunset_css', 'sunset_theme_settings_page');
 }
 add_action('admin_menu', 'sunset_add_admin_page');
 
 function sunset_theme_create_page() {
   // generation of admin page
+  require_once(get_template_directory() . '/inc/templates/sunset-admin.php');
+}
+
+function sunset_theme_settings_page() {
+  // generation of admin sub page
+  echo '<h1>Sunset Custom CSS</h1>';
 }
---
+++ inc/templates/sunset-admin.php
@@ -0,0 +1,4 @@
+<h1>Sunset Theme Options</h1>
+<h3 class="title">Manage Options</h3>
+<p>Customize the default WordPress Appearance Options</p>
+<?php bloginfo('name');
