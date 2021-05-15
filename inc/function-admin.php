40cd6665555962604c06cbfa558059c75259713a
Genesis Gabiola <gbgabiola@gmail.com>
2021-05-15 16:13:16 +0800

    Create custom options with Settings API

--- inc/function-admin.php
+++ inc/function-admin.php
@@ -12,10 +12,28 @@ function sunset_add_admin_page() {
 
   // Generate Sunset Admin Sub Pages
   add_submenu_page('gbgabiola_sunset', 'Sunset Theme Options', 'General', 'manage_options', 'gbgabiola_sunset', 'sunset_theme_create_page');
-
   add_submenu_page('gbgabiola_sunset', 'Sunset CSS Options', 'Custom CSS', 'manage_options', 'gbgabiola_sunset_css', 'sunset_theme_settings_page');
+
+  // Activate custom settings
+  add_action('admin_init', 'sunset_custom_settings');
 }
 add_action('admin_menu', 'sunset_add_admin_page');
+
+
+function sunset_custom_settings() {
+  register_setting('sunset-settings-group', 'first_name');
+  add_settings_section('sunset-sidebar-options', 'Sidebar Options', 'sunset_sidebar_options', 'gbgabiola_sunset');
+  add_settings_field('sidebar-name', 'First Name', 'sunset_sidebar_name', 'gbgabiola_sunset', 'sunset-sidebar-options');
+}
+
+function sunset_sidebar_options() {
+  echo 'Customize your Sidebar Information';
+}
+
+function sunset_sidebar_name() {
+  $firstName = esc_attr(get_option('first_name'));
+  echo '<input type="text" name="first_name" value="' . $firstName . '" placeholder="First Name" />';
+}
 
 function sunset_theme_create_page() {
   // generation of admin page
--- inc/templates/sunset-admin.php
+++ inc/templates/sunset-admin.php
@@ -1,4 +1,9 @@
 <h1>Sunset Theme Options</h1>
-<h3 class="title">Manage Options</h3>
-<p>Customize the default WordPress Appearance Options</p>
-<?php bloginfo('name');
+<?php settings_errors(); ?>
+<form action="options.php" method="post">
+  <?php
+  settings_fields('sunset-settings-group');
+  do_settings_sections('gbgabiola_sunset');
+  submit_button();
+  ?>
+</form>
